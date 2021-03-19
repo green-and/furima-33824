a2439e3d6b145bd51259f7651bad4c0e8250eec4
green-and <t.nakamura.official@gmail.com>
2021-03-19 17:55:09 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,17 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
-  belongs_to :psotage
+  belongs_to :postage
   belongs_to :sender_area
-  belongs_to :require_time
+  belongs_to :required_time
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 }
+  validates :condition_id, presence: true, numericality: { other_than: 1 }
+  validates :postage_id, presence: true, numericality: { other_than: 1 }
+  validates :sender_area_id, presence: true, numericality: { other_than: 1 }
+  validates :required_time_id, presence: true, numericality: { other_than: 1 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
