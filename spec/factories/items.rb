22448e32c0f81d8de35f21cde4e480f61d4fc19a
green-and <t.nakamura.official@gmail.com>
2021-03-19 20:20:10 +0900

    単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name              { Faker::Lorem.word }
+    explanation       { Faker::Lorem.sentences }
+    category_id       { 2 }
+    condition_id      { 2 }
+    postage_id        { 2 }
+    sender_area_id    { 2 }
+    required_time_id  { 2 }
+    price             { 300 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image_png')
+    end
   end
 end
