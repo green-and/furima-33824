0d63d6c6f80c8495558ff409f8d9022a10d318b7
green-and <t.nakamura.official@gmail.com>
2021-03-23 20:00:45 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
   root to: "items#index"
 end
