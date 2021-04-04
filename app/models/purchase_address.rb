014c80491c5889e88c4e6c528f2957b3b69539b3
green-and <t.nakamura.official@gmail.com>
2021-04-04 12:11:32 +0900

    バリデーションの追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -5,10 +5,10 @@ class PurchaseAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postcode
+    validates :postcode, format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :municipality
     validates :address
-    validates :phone_number
+    validates :phone_number, format: {with: /\A\d{10,11}\z/ , message: "is invalid."}
   end
   validates :prefecture_id, numericality: { other_than: 1 }
 
