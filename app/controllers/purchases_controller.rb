ec95419ec1a985bbeed903014395d27d99457023
green-and <t.nakamura.official@gmail.com>
2021-04-04 17:08:05 +0900

    条件分岐の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,8 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :current_item_user, only: [:index, :create]
+  before_action :item_soldout, only: [:index, :create]
+
   def index
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
@@ -17,5 +21,19 @@ class PurchasesController < ApplicationController
   private
   def purchase_params
     params.require(:purchase_address).permit(:user_id, :item_id, :postcode, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def current_item_user
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def item_soldout
+    @item = Item.find(params[:item_id])
+    if Purchase.exists?(@item.id)
+      redirect_to root_path
+    end
   end
 end
