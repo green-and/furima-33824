43b4409fd289f00800270b745f7ebf6ce47639e2
green-and <t.nakamura.official@gmail.com>
2021-04-05 18:53:14 +0900

    サーバーサイドのトークン取得の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -20,7 +20,7 @@ class PurchasesController < ApplicationController
 
   private
   def purchase_params
-    params.require(:purchase_address).permit(:user_id, :item_id, :postcode, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_address).permit(:user_id, :item_id, :postcode, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def current_item_user
